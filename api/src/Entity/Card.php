b78c12541920e4f5d991526e2acedbf9cf8a6c37
konta <farod82315@downlor.com>
2025-01-26 15:20:27 +0100

    Add card entity

---
+++ api/src/Entity/Card.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CardRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+#[ApiResource]
+class Card
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $baseCoolDownInMs = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBaseCoolDownInMs(): ?int
+    {
+        return $this->baseCoolDownInMs;
+    }
+
+    public function setBaseCoolDownInMs(int $baseCoolDownInMs): static
+    {
+        $this->baseCoolDownInMs = $baseCoolDownInMs;
+
+        return $this;
+    }
+}
